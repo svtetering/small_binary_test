f16ac9b2dadceff27bd391ff8fc666ebd8d5cfa5
Steven van Tetering <steven@tikveel.nl>
2021-07-26 16:37:01 +0200

    write() works

--- src/C/demo.c
+++ src/C/demo.c
@@ -4,15 +4,17 @@ extern void _sys_exit();
 extern void _sys_write(int fd, char* buf, int count);
 extern int _sys_open(char* filename, int flags, umode_t mode);
 
-#define O_WRONLY 0x0001
-#define O_CREAT 0x0100
+#define O_RDONLY    00000000
+#define O_WRONLY    00000001
+#define O_CREAT     00000100
+#define O_TRUNC     00001000
 
 int entry() {
     char path[] = "./test";
-    int fd = _sys_open(path, O_WRONLY | O_CREAT, 0);
+    int fd = _sys_open(path, O_WRONLY | O_CREAT | O_TRUNC, 0);
 
-    char test[] = "LETS FRICKING GOOOOOO\n";
-    _sys_write(fd, test, sizeof(test));
+    char test[] = "Does this work?";
+    _sys_write(fd, test, sizeof(test) - 1);
 
     _sys_exit();
 }
